c9af33b2f68135c6f45ce43d08d7ce830f529ec5
dimesangel <angelguillermo.velasquez@salud.gob.sv>
2025-04-06 10:35:55 -0600

    second commit

---
+++ Shared.Infrastructure/db/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shared.Infrastructure.db
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
